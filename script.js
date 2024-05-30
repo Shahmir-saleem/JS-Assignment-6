03ab209bf1f970a7a78017469f5e09f9a28d9f46
Shahmir-saleem <157698728+Shahmir-saleem@users.noreply.github.com>
2024-05-30 15:29:52 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+var vowel = prompt('Enter your first letter')
+vowel =('a')
+vowel =('e')
+vowel =('i')
+vowel =('o')
+vowel =('u')
+if (vowel){console.log("this is vowel letter");}
+else (document.write("this is not vowel letter"))
